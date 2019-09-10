9a61070d6a0a6664634d84302ce440beb6b68db4
Patrick Whelan <patrickawhelan@gmail.com>
2019-09-10 15:38:21 -0700

    With starter code

---
+++ public/main.js
@@ -0,0 +1,32 @@
+let appendLocationsArrayToHtml = (locations) => {
+  let locationList = document.getElementById('locations')
+
+  locations.forEach(location => {
+    locationList.innerHTML += `<li>${location.city}, ${location.country}</li>`
+  })
+}
+
+let fetchLocations = () => {
+  // Your code here
+}
+
+let postLocation = (event) => {
+  event.preventDefault()
+
+  let cityInputField = document.getElementById('city')
+  let countryInputField = document.getElementById('country')
+
+  let newLocation = {
+    location: {
+      city: cityInputField.value,
+      country: countryInputField.value
+    }
+  }
+  // Your code here
+}
+
+fetchLocations()
+
+document
+  .getElementById('new-location-submit-button')
+  .addEventListener('click', postLocation)
